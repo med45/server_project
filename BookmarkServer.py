6f11828600e862ee792a4aa4405cbb776bd8aa09
med45 <33964650+med45@users.noreply.github.com>
2018-12-08 12:38:58 +0100

    Update BookmarkServer.py

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # A *bookmark server* or URI shortener.
-
+import os
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
@@ -106,6 +106,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
